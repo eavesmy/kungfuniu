4445783f9e838c15b35f98bbc0c66f0c54906f4b
Gary Zhang <eavesmy@gmail.com>
2017-12-07 22:32:23 +0800

    Update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { HomeModule } from './app/home/home.module'
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -10,4 +9,4 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  .catch(err => console.warn(err));
